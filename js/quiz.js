a7eed315e51b6ef2a29cc63d83aff1680eaf9d62
HamzahFar1203 <mhfarooqui203@gmail.com>
2026-05-24 23:03:54 -0500

    Updated the comments in quiz.js

--- js/quiz.js
+++ js/quiz.js
@@ -1,8 +1,3 @@
-/*
-    TO DO: Look over code and clean it as necessary!
-    TO DO: Add better comments!
-*/
-
 // HTML Elements
 const question = document.getElementById("question");
 const option_one = document.getElementById("option-one");
@@ -15,14 +10,24 @@ const questions_data = JSON.parse(sessionStorage.getItem("quiz_questions"));
 const total_questions = questions_data.length;
 var score = 0;
 
-// Generates a random index and collects the question at that index from questions_data
+// Saves the question object returned by the nextQuestion function
 var current_question = nextQuestion();
 
+// When the page loads, the first selected question is displayed
 window.onload = function () {
     displayQuestion();
 }
 
-// Event listeners for each quiz option
+/*
+
+    EVENT LISTENERS FOR THE THREE OPTIONS FOR EACH QUIZ QUESTION
+    
+    Each event listener checks if that options status property is
+    equal to true. If it is, then add +1 to the score. Then pick
+    the next question and display it.
+
+*/
+
 option_one.addEventListener("click", () => {
 
     if (current_question.question_option_one.status === true) {
@@ -69,6 +74,7 @@ function displayQuestion () {
 
 // Picks a new question, and checks if the app has hit the end of the array
 function nextQuestion () {
+
     if (questions_data.length !== 0) {
 
         // Picks a random question from the questions_data array
@@ -82,8 +88,10 @@ function nextQuestion () {
 
     } else {
 
+        // If there are no more questions left, run this:
         sessionStorage.setItem("quiz_score", score);
         window.location = "../results.html";
 
     }
+
 }
